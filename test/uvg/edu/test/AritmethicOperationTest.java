a0f2dd49f85668abdc61e20f57ff40060a718f3b
wachuuu <88355602+Wachuuu15@users.noreply.github.com>
2022-03-23 22:14:42 -0600

    pruebas unitarias

--- test/uvg/edu/test/AritmethicOperationTest.java
+++ test/uvg/edu/test/AritmethicOperationTest.java
@@ -12,7 +12,7 @@ import uvg.edu.lisp.Interpreter;
 
 
 /**
- * @author jenni
+ * @author jenny
  *
  */
 class AritmethicOperationTest {
@@ -35,15 +35,82 @@ class AritmethicOperationTest {
 	}
 	
 	
-	
+	@Test
 		void subtractionOperation() {
 		Inicio();
 		IOperationResult result =  intp.subtractionOperation("(- 5 2)");
-		assertEquals (result.performOperation(),"El resultado de la operacion  suma  es: 3" );
+		assertEquals (result.performOperation(),"El resultado de la operacion  resta  es: 3" );
 	}
 	
+	@Test
+	//jUnit Interpreter
+		void multiplicationOperation() {
+		Inicio();
+		IOperationResult result =  intp.multiplicationOperation("(* 2 2 2)");
+		assertEquals (result.performOperation(),"El resultado de la operacion  multiplicacion  es: 8" );
+	}
 	
+	@Test
+	//jUnit Interpreter
+		void divisionOperation() {
+		Inicio();
+		IOperationResult result =  intp.divisionOperation("(/ 12 4)");
+		assertEquals (result.performOperation(),"El resultado de la operacion  division  es: 3" );
+	}
 	
+	@Test
+	//jUnit Interpreter
+		void equal() {
+		Inicio();
+		IOperationResult result =  intp.equal("(= 100 200)");
+		assertEquals (result.performOperation(),"El resultado de la operacion  equal  es: NIL" );
+	}
+	
+	@Test
+	//jUnit Interpreter
+		void greaterThan() {
+		Inicio();
+		IOperationResult result =  intp.greaterThan("> 100 200");
+		assertEquals (result.performOperation(),"El resultado de la operacion  greater than  es: NIL" );
+	}
+	
+	@Test
+	//jUnit Interpreter
+		void smallerThan() {
+		Inicio();
+		IOperationResult result =  intp.smallerThan("(< 100 200)");
+		assertEquals (result.performOperation(),"El resultado de la operacion  smaller than  es: T" );
+	}
+	
+	@Test
+	//jUnit Interpreter
+		void quote() {
+		Inicio();
+		IOperationResult result =  intp.quote("('(1 2))");
+		assertEquals (result.performOperation(),"El resultado de la operacion  es: (1 2)" );
+	}
+	
+	@Test
+	//jUnit Interpreter
+			void variableAssigment() {
+			Inicio();
+			IOperationResult result =  intp.variableAssigment("( +a )");
+			assertEquals (result.performOperation(),"Variable:  asignada con valor 0" );
+		}
+	
+	@Test
+	void listOperation() {
+	Inicio();
+	IOperationResult result =  intp.listOperation("(list 1 2 NIL)");
+	assertEquals (result.performOperation(),"El resultado de la operacion  list  es: (1 2 NIL)" );
+}
+	
+	@Test
+	void atomOperation() {
+	Inicio();
+	IOperationResult result =  intp.atomOperation("(atom '(1 T NIL))");
+	assertEquals (result.performOperation(),"El resultado de la operacion  atom  es: NIL" );
+}
 	
 
 }
